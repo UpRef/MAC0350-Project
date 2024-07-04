8a997e524d209c0daca75e5cb57e6706035caa79
Lucas Sobrinho <lucasmsobrinho@gmail.com>
2024-07-04 12:11:38 -0300

    updates on schemas and routes

--- front/search_tool/seach_script.js
+++ front/search_tool/seach_script.js
@@ -1,46 +1,64 @@
-// Fetch JSON data from a file and use it
-/*
-fetch('./search_result/search_results.json')
-  .then(response => response.json())
-  .then(json => {
-    const el = document.querySelector(".root-1");
-    var tt1 = new table(el);
-    tt1.addData(json);
-    tt1.interactivity(); // Ensure any interactivity is initialized after data is loaded
-  })
-  .catch(error => console.error('Error loading JSON data:', error));
-*/
+searchform = document.querySelector("#search-form")
 
-searchform = document.querySelectorById("search-form")
+EventListener('DOMContentLoaded', function() {
+    const searchForm = document.querySelector("#search-form");
+  
+    searchForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        alert("alert");
+        search();
+    });
+})
 
-searchform.setAttribute("onsubmit", "search()");
-console.log('AAAAAAAAAAAAA')
 search = function () {
-  fetch('http://127.0.0.1:8080/folders/from/user/42', {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append('Accept', 'application/json');
+    
+    const category = document.querySelector("#selected-category").textContent
+    const query = document.querySelector("#search-input").value
+    console.log(category, query)
+    url = 'http://127.0.0.1:8080/search/' + category + '/' + query
+    
+    const request = {
       method: 'GET',
-      mode: 'no-cors',
-      'Access-Control-Allow-Origin': '*'
-  }) // Fetch JSON file
-  .then(response => response.json()) // Parse JSON
-  .then(data => {
-    console.log("get")
-      const foldersList = document.getElementById('folders-list');
-      data.folders.forEach(folder => {
-          const li = document.createElement('li');
-          const a = document.createElement('a');
-          a.setAttribute('onclick', 'loadFolderPage()');
-          a.setAttribute('href', `#${folder.name}`);
-          a.textContent = folder.name;
-          li.appendChild(a);
-          foldersList.appendChild(li);
-      });
+      headers: myHeaders,
+      redirect: "follow"
+    }
 
-      const li = document.createElement('li');
-      const a = document.createElement('a');
-      a.setAttribute('onclick', 'addFolder()');
-      a.setAttribute('href', `#`);
-      a.textContent = "+ adicionar paper"
-      li.appendChild(a);
-      foldersList.appendChild(li);
-  })
+    fetch(url, request)
+    .then(response => response.json()) // Parse JSON
+    .then(data => {
+        loadFolderPage(query, category)
+        writeResultsPage(data)
+    })
 };
+
+
+function loadFolderPage(query, category) {
+  fetch('./search_result/demo.html')
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById('content').innerHTML = data;
+      })
+      .catch(error => console.error('Deu erro :(', error));   
+}
+
+function writeResultsPage(paperList) {
+  let results_div = document.querySelector("#results");
+  let title = document.querySelector("#folder-name");
+  title.innerText = 'Resultados da busca por ('+category+'): ' + query
+
+  var results = ""
+
+  paperList.forEach((paper) => {
+      results += '<div class="resultado" style="background-color: #ccc;">'
+          +'<div class="title"><b>title</b>: '+paper.title+'</div>'
+          +'<div class="authors"><b>authors</b>: '+paper.authors+'</div>'
+          +'<div class="abstract"><b>abstract</b>: '+paper.abstract+'</div>'
+          +'<div><span>remove</span></div>'
+          +'</div><div style="background-color: "#fff"><br></div>';
+  })
+
+  results_div.innerHTML = results
+}
